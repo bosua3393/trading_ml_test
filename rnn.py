89cc5f57ad5647bae13e9fbf64b868dfd5c877b3
bosua3393@gmail.com <bosua3393@gmail.com>
2019-03-31 14:58:56 -0400

    lstm try

--- rnn.py
+++ rnn.py
@@ -1,9 +1,13 @@
-import tensorflow as tf
 import pandas as pd
 import numpy as np
 from data import btc_data
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, TimeDistributedDense
+from tensorflow.keras import optimizers
+
 
 FUTURE = 24
+
 
 def label(current, future):
     if future > current:
@@ -11,15 +15,32 @@ def label(current, future):
     else:
         return 0
 
+
 data = pd.DataFrame({'btc_usd': np.flip(btc_data.data)})
 data['norm_change'] = 1-(data['btc_usd'].shift(1)/data['btc_usd'])
 data['norm_change'] = data['norm_change']/(abs(data['norm_change']).max())
 data['future'] = data['btc_usd'].shift(-FUTURE)
 data = data[:-FUTURE]
 data['label'] = list(map(label, data['btc_usd'], data['future']))
-print(data)
+data = data.dropna()
 
-batch_x = data['norm_change'].values
-batch_y = data['label'].values
+batch_x = np.array(data['norm_change'].values)
+batch_y = np.array(data['label'].values)
+
+batch_x = batch_x.reshape(1, 14949, 1)
+batch_y = batch_y.reshape(1, 14949, 1)
+
+print(batch_x, batch_y)
 
 
+model = Sequential()
+
+model.add(LSTM(128, input_shape=(14949, 1), return_sequences=True))
+
+model.add
+
+opt = optimizers.Adam(lr=0.0001, decay=.000001)
+
+model.compile(optimizer=opt, loss='sparse_categorical_crossentropy')
+
+model.fit(batch_x, batch_y, epochs=5)
