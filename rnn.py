40e586ae540194c6d816470093d08ce67d85c486
yui-sub <bosua3393@gmail.com>
2019-04-01 18:24:46 -0400

    TimedistributesDense added

--- rnn.py
+++ rnn.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 from data import btc_data
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM
+from tensorflow.keras.layers import Dense, LSTM, TimeDistributed
 from tensorflow.keras import optimizers
 
-
 FUTURE = 24
+length = 14949
 
 
 def label(current, future):
@@ -27,21 +27,23 @@ data = data.dropna()
 batch_x = np.array(data['norm_change'].values)
 batch_y = np.array(data['label'].values)
 
-batch_x = batch_x.reshape(14949, 1, 1)
-batch_y = batch_y.reshape(14949, 1)
+batch_x = batch_x.reshape(1, length, 1)
+print(batch_x)
+batch_y = batch_y.reshape(1, length, 1)
 
 model = Sequential()
 
-model.add(LSTM(128, input_shape=(1, 1), return_sequences=True, activation='relu'))
+model.add(LSTM(128, input_shape=(length, 1), return_sequences=True))
 
-model.add(LSTM(128, input_shape=(1, 1), return_sequences=False, activation='relu'))
-
-model.add(Dense(128, activation='relu'))
-
-model.add(Dense(1, activation='softmax'))
+model.add(TimeDistributed(Dense(1, activation='softmax')))
 
 opt = optimizers.Adam(lr=.001, decay=.000001)
 
 model.compile(optimizer=opt, loss='mean_squared_error', metrics=['accuracy'])
+'''
+model.fit(batch_x, batch_y, epochs=5, verbose=1)
 
-model.fit(batch_x, batch_y, epochs=5)
+result = model.predict(batch_x)
+for i in range(len(result[0])):
+    print(result[0][i])
+'''
